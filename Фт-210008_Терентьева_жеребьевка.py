53844fedeadea186e00f49026dbe1f2018ada281
Katerina001Tr <93012937+Katerina001Tr@users.noreply.github.com>
2022-11-05 20:26:35 +0500

    добавляем цикл и ввод значения

--- Фт-210008_Терентьева_жеребьевка.py
+++ Фт-210008_Терентьева_жеребьевка.py
@@ -1,2 +1,9 @@
 import random
 import logging
+while True:
+    logging.basicConfig(filename="ex.log", level=logging.INFO)
+    logging.info("Programm started")
+    try:
+        n = int(input("Сколько бочонков необходимо вытянуть из мешка?"))
+        logging.info(f"Users nuber is {n}")
+
